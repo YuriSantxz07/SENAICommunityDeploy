b6c8f21c8b8ab76aea6d1d991b0c743f70722a1e
YuriSantxz07 <yurisantsboy2007@gmail.com>
2025-11-14 16:53:37 -0300

    upd

--- frontend/Principal/JS/perfil.js
+++ frontend/Principal/JS/perfil.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // --- CONFIGURAÇÕES E VARIÁVEIS GLOBAIS ---
-  const backendUrl = "http://localhost:8080";
+  const backendUrl = "https://senaicommunitydeploy-production.up.railway.app";
   const jwtToken = localStorage.getItem("token");
   const defaultAvatarUrl = `${backendUrl}/images/default-avatar.jpg`;
   let stompClient = null;
